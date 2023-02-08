ceb02346fa7dcc2e172e4e061d3825ffbba2ee70
Marcos Vinícius Silva <m_vinicius94@outlook.com>
2023-02-08 11:26:29 -0300

    Inserido função para verificar disponibilidade de livros

--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -2,10 +2,13 @@ const elementoParaInserirLivros = document.getElementById('livros');
 
 function exibirOsLivrosNaTela(listaDeLivros) {
     elementoParaInserirLivros.innerHTML = '';
+
     listaDeLivros.forEach(livro => {
+        //let disponibilidade = verificarDisponibilidadeDoLivro(livro);
+        let disponibilidade = livro.quantidade > 0 ? 'livro__imagens' : 'livro__imagens indisponivel';
         elementoParaInserirLivros.innerHTML += `
         <div class="livro">
-        <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
+        <img class="${disponibilidade}" src="${livro.imagem}" alt="${livro.alt}" />
         <h2 class="livro__titulo">
             ${livro.titulo}
         </h2>
@@ -18,3 +21,11 @@ function exibirOsLivrosNaTela(listaDeLivros) {
         `
     })
 }
+
+// function verificarDisponibilidadeDoLivro(livro) {
+//     if (livro.quantidade > 0) {
+//         return disponibilidade = 'livro__imagens';
+//     } else {
+//         return disponibilidade = 'livro__imagens indisponivel';
+//     }
+// }
